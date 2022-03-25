753014a57c266dded63b12ec8cbc251400f1412c
DESKTOP-RQFSSPN\이소민 <thals0107@naver.com>
2022-03-25 17:23:17 +0900

    모각코 파이썬 크롤링 9일차
    정적크롤링
    URL 주소의 패턴을 활용해 여러 페이지의 정보 수집
    G마켓 원하는 각 상품의 이름과 가격 수집

---
+++ 모각코/day9.py
@@ -0,0 +1,21 @@
+import requests 
+import bs4
+
+while True:
+    keyword=input("검색을 원하는 키워드를 입력하세요.(0을 입력하면 종료)")
+    if keyword =="0":
+        break
+
+    URL = "https://browse.gmarket.co.kr/search?keyword="+keyword 
+    raw=requests.get(URL)
+    html=bs4.BeautifulSoup(raw.text,'html.parser')
+    box=html.find('div',{'class':'section__module-wrap','module-design-id':'15'})
+    items=box.find_all('div',{"class":'box__item-container'})
+
+    print("<G마켓의",keyword,"상품 정보>")
+    for item in items[:3]:
+        title=item.find('span',{'class':'text__item'})
+        price=item.find('strong',{'class':'text__value'})
+        print("이름:",title.text)
+        print("가격:",price.text)
+        print()
